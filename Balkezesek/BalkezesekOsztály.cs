bc32f657c793c83e0c349ec1609f41a8403e92b4
Mónus Bence <monusbence3@gmail.com>
2023-05-25 11:48:50 +0200

    4.óra vége!

---
+++ Balkezesek/BalkezesekOsztály.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    internal class BalkezesekOsztály
+    {
+        string nev;
+        string elsoPalyaraLepes;
+        string utolsoPalyaraLepes;
+        int suly;
+        int magassag;
+
+        public BalkezesekOsztály(string nev, string elsoPalyaraLepes, string utolsoPalyaraLepes, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elsoPalyaraLepes = elsoPalyaraLepes;
+            this.utolsoPalyaraLepes = utolsoPalyaraLepes;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev; }
+        public string ElsoPalyaraLepes { get => elsoPalyaraLepes; }
+        public string UtolsoPalyaraLepes { get => utolsoPalyaraLepes; }
+        public int Suly { get => suly; }
+        public int Magassag { get => magassag; }
+    }
+}
--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        static List<BalkezesekOsztály> lista = new List<BalkezesekOsztály>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Olvasas();
+            HarmasFeladat();
+        }
+
+        static void Olvasas()
+        {
+            StreamReader szoveg = new StreamReader("balkezesek.csv");
+            lista.Skip(1);
+            while (!szoveg.EndOfStream)
+            {
+                string[] player = szoveg.ReadLine().Split(";");
+                
+            }
+
+            szoveg.Close();
+        }
+        static void HarmasFeladat()
+        {
+
+            Console.WriteLine(lista.Count);
         }
     }
 }
